00303bbc5281cea2bf697d50599ed5338ab2ef7f
bellygoblin <brian@mitchellfamily.me>
2024-11-12 18:52:14 -0500

    Implement group containers

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updatable:
+            obj.update(dt)
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
--- player.py
+++ player.py
@@ -36,7 +36,7 @@ class Player(CircleShape):
 
     if keys[pygame.K_s]:
        self.move(-dt)
-       
+
   def move(self, dt):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
     self.position += forward * PLAYER_SPEED * dt
